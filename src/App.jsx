29a0e4de26e2f44c7be88c6595d39192bdc3a522
Kesley David <kesleygti@gmail.com>
2023-07-08 13:42:10 -0300

    feat: test 1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-col justify-center items-center text-center gap-8'>
+
+      <img className='w-[50px] h-[50px]' src="https://safebiolink.com/uploads/avatars/eafdd92fb78cb06b92eb718455065664.png" alt="" />
+      <h1 className="text-3xl font-bold underline">
+        Kesley DEV
+      </h1>
+
+      <a className='p-4 bg-white rounded-md' href="https://kesley.dev">website</a>
+
+      <a className='p-4 bg-white rounded-md' href="https://www.inflncrwrd.com/52ATYK8BF4">link safe 1</a>
+      <a className='p-4 bg-white rounded-md' href="https://www.inflncrwrd.com/DFXG2Q451J">link safe 2</a>
+      <a className='p-4 bg-white rounded-md' href="https://www.inflncrwrd.com/8NWHDA7QBK">link safe 3</a>
+    </div>
+  )
+}
+
+export default App
